526a4197319d1d6c4470dc1212a47ab8aa7090da
Vagelis Kosmiadis <97461782+kosmiadis@users.noreply.github.com>
2024-01-14 10:32:08 +0200

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 module.exports.email = '[your email]'
 module.exports.password = '[your password]'
 module.exports.uri = "[your uri]"
-=======
-export const email = '[your email]@gmail.com'
-export const password = '[your password]'
->>>>>>> 383033f92767f7f379dc3977e81857250280df86
